f7d4a4e8bed435fccd051db8917105f99fd87ad8
MaltaProject <nicholas.ingalls@gmail.com>
2012-10-22 16:30:54 -0300

    Removed old code

--- Zaot Empire/src/Main/chatServer.java
+++
@@ -1,8 +0,0 @@
-package Main;
-
-//When I figure out how to do it, the chat server will go here
-//The chat server will use event firing based on a millisecond timer
-
-public class chatServer {
-
-}
